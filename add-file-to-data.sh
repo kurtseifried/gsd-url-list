eecdfb6ccea3543da51ec4fe1485b9db8d54e926
Kurt Seifried <kurt@seifried.org>
2021-06-17 10:33:43 -0600

    Create data files

---
+++ add-file-to-data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# Takes one arg, a filename
+# Reads the file line by line
+# splits on the hostname
+# writes the line to the data/hostname file
+# then sorts and uniqs all the files
+#
+
+args=("$@")
+
+filename=${args[0]}
+
+while IFS= read -r line; do
+    hostname=`echo $line | cut -d'/' -f3`
+    echo $line >> data/$hostname
+done < $filename
+
+# After done sort and uniq the files
+
+cd data
+
+for i in *; do
+  sort $i | uniq > $i.239f87h2983ghy2938gh
+  mv -f $i.239f87h2983ghy2938gh $i
+done
+
+cd ..
+
+echo "DONE"
