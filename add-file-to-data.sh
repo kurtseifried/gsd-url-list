e486078ce1c6d8019dacc2fd29ca3e701fbbb40f
Kurt Seifried <kurt@seifried.org>
2021-06-28 12:33:43 -0600

    updated notes

--- add-file-to-data.sh
+++ add-file-to-data.sh
@@ -6,7 +6,8 @@
 # splits on the hostname
 # writes the line to the data/hostname file
 # then sorts and uniqs all the files
-#
+# creates new hostname if file does not exist
+# Writes full URL to the file
 
 args=("$@")
 
@@ -15,6 +16,7 @@ filename=${args[0]}
 while IFS= read -r line; do
     hostname=`echo $line | cut -d'/' -f3`
     echo $line >> data/$hostname
+    # see if it already exists or not, if not exists print it out?
 done < $filename
 
 # After done sort and uniq the files
@@ -26,6 +28,6 @@ for i in *; do
   mv -f $i.239f87h2983ghy2938gh $i
 done
 
-cd ..
+#cd ..
 
 echo "DONE"
